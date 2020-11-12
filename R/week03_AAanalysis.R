33a98ac848ba6d38f2a1ecb54cd9e59ae39e9f1a
Gillian McGinnis <gmcginnis@rstudio.reed.edu>
2020-11-11 22:39:49 -0800

    Added comments. Began conducting more data analysis.

--- R/week03_AAanalysis.R
+++ R/week03_AAanalysis.R
@@ -1,30 +1,38 @@
 ## Week 03 Data Analysis for AA data
 ## Gillian McGinnis
 ## Created 06 November 2020
-## Updated 06 November 2020
+## Updated 11 November 2020
 
+library(tidyverse)
+library(ggthemes)
 source("R/week02_fullTidy.R")
 rm(list = setdiff(ls(), c("AA_merged", "dataKey_tidy")))
 
+## Adjusting dataframes
 dataAA <- AA_merged %>%
   mutate(xRsd = as.numeric(as.character(case_when(xRsd != "HIGH" ~ xRsd))))
 dataKey <- dataKey_tidy
 
+## Cleaning environment
 rm(list = setdiff(ls(), c("dataAA", "dataKey")))
 
+## Creating a list of sample sites by excluding NAs and Method Blanks
 sampleSites <- unique(filter(dataAA, site != "MB", site != "")$site)
 
+## Changing names back to snake_case
 sample_key <- dataKey %>%
   clean_names()
 
+## Chancing names back to snake_case and renaming a variable to match that of other similar dataframes
 data_aa <- dataAA %>%
   clean_names() %>%
   mutate(rsd = x_rsd)
 
 
+## Code based on ICPMS analysis
 # Initiate loop, filter out cal data
 calAA <- NULL
-
+## Selecting cal standards
 cal <- data_aa %>%
   filter(type == "CalStd" | type == "CalStd2" | type =="CalStd4") %>%
   select(concentration, mean_abs, rsd)
@@ -44,8 +52,21 @@ plot(cal$mean_abs ~ cal$concentration,
      ylab = "Counts per second")+
   abline(model, col = "red")+
   title(paste("Calibration for Cr"))
+
+## Alternatively, here's my way.
+myResults <- summary(lm(mean_abs ~ concentration, weights = w, data = cal))
+
+ggplot(cal, aes(x = concentration, y = mean_abs))+
+  geom_point(shape = 1)+
+  geom_smooth(method = "lm", se = FALSE, color = "red")+
+  theme_few()+
+  labs(x = "Concentration of Cr (ppb)", y = "Counts per second", title = "Calibration curve for Cr")
   
+## Back to some given stuff:
 equation <- tibble(metal = slope, slopeStd, intercept, interceptStd)
 calAA <- rbind(calAA, equation)
 
 remove(equation, cal, slope, slopeStd, intercept, interceptStd, w, model, uniqueMetal)
+
+
+## Step 4.1: creating a function to analyze the samples
