b228f0a170f56d9f65b4168ec6ba92b94a1dd2f7
Gillian McGinnis <gmcginnis@rstudio.reed.edu>
2020-11-16 19:31:46 -0800

    Created function to run series of t-tests by location

--- R/stats_Cr.R
+++ R/stats_Cr.R
@@ -32,5 +32,31 @@ cr_aa <- sample_data_AA %>%
 
 cr_all <- full_join(cr_icpms, cr_aa)
 
-t_test_function
+sample_sites <- unique(cr_all$site)
+#Input: location
+# Output: df of t.test results
+t_df <- NULL
+t_test_function <- function(site_input){
+  tidied <- tidy(t.test(conc_blanked ~ instrument, data = subset(cr_all, site == site_input))) %>%
+    as.data.frame() %>%
+    mutate(site = site_input)
+  
+  t_df <<- rbind(t_df, tidied)
+}
 
+run_sites <- function(Function){
+  value <- NULL
+  for(sites in sample_sites){
+    site_value <- Function(sites)
+    value <- rbind(site_value, value)
+  }
+  return(value)
+}
+
+run_sites(t_test_function)
+
+t_df <- t_df %>%
+  mutate(label = case_when(
+    p.value < 0.05 ~ "p < 0.05", # Reject null hypothesiss; diff is significant
+    p.value >= 0.05 ~ "Non-Sig" # Fail to reject null hyp; diff is not significant
+  ))
